654e851bbb47c2763dbcf0f9a9b76c79315485a2
Abdul Ghaffar <ghaffarbajwa9@gmail.com>
2023-11-30 08:38:58 +0500

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-AdminUser.create!(email: 'ghaffarbajwa9@gmail.com', password: '11223344', password_confirmation: '11223344') if Rails.env.development?
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'ghaffarbajwa9@gmail.com', password: '11223344', password_confirmation: '11223344') if Rails.env.development?
+AdminUser.create!(email: 'ghaffarbajwa9@gmail.com', password: '11223344', password_confirmation: '11223344')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
